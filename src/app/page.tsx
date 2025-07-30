e4480a2f266b2dbe6ae01f399bdeb64f9b49dac3
Mskerz <wichasin.s@gmail.com>
2025-07-30 18:31:02 +0700

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import CategoriesListButton from "@/components/button/categories";
 import { SongClient } from "@/components/data/song";
-import { SongClientWrapper } from "@/components/wrapper";
 import { getCategory } from "@/libs/api/category";
-import { useGetSongsQuery } from "@/libs/rtk/song";
 
 export default async function HomePage() {
   const categories = await getCategory();

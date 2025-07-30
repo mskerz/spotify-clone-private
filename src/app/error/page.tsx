e5c91163b90fbf712d43a2460355e68cec4a985f
Mskerz <wichasin.s@gmail.com>
2025-07-30 19:54:09 +0700

    remove error page

--- src/app/error/page.tsx
+++
@@ -1,9 +0,0 @@
-function page() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold text-red-600 mb-4">Something went wrong</h1>
-            <p className="text-lg text-gray-700">Sorry, an unexpected error has occurred.</p>
-        </div>
-    );
-}
-export default page

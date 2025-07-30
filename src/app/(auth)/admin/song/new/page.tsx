9f81239c6f7f11a4701f7a6a12dfe9d2e5eb3d81
Mskerz <wichasin.s@gmail.com>
2025-07-30 19:08:16 +0700

    fix bug error prod : Export encountered an error on /(auth)/admin/dashboard/songs/new/page: /admin/dashboard/songs/new, exiting the build.

--- src/app/(auth)/admin/song/new/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SongForm } from "@/components/form";
-import { getCategory } from "@/libs/api/category";
-
-async function page() {
-  const categories = await getCategory();
-  return <SongForm categories={categories} />;
-}
-export default page;
